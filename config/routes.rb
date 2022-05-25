4dbdf8a64901d57a2b9231687fb2a97759fde4b4
Frechfuchs <dev.frechfuchs@gmail.com>
2022-05-25 16:32:32 +0200

    User Index

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,11 @@ Rails.application.routes.draw do
     # Redirests signing out users back to sign-in
     get "users", to: "devise/sessions#new"
   end
-
+  
+  #match '/users',   to: 'users#index',   via: 'get'
   devise_for :users
-
+  get 'users/index'
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
