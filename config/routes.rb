49ea366c02be6906f2a7d6f1b1a521cc771167e6
Frechfuchs <dev.frechfuchs@gmail.com>
2022-05-25 17:23:18 +0200

    Add Home Page

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,10 @@
 Rails.application.routes.draw do
-  devise_scope :user do
-    # Redirests signing out users back to sign-in
-    get "users", to: "devise/sessions#new"
-  end
   
   #match '/users',   to: 'users#index',   via: 'get'
   devise_for :users
-  get 'users/index'
+  get "users/index"
   
+  root "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
