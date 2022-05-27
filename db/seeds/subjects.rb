8787fe2170c19bce71c258fad68915571602c424
Frechfuchs <dev.frechfuchs@gmail.com>
2022-05-27 02:14:13 +0200

    Render deeply nested json objects

--- db/seeds/subjects.rb
+++ db/seeds/subjects.rb
@@ -1,9 +1,10 @@
 def seed_subjects
   5.times do |num|
-    subject = Subject.create!(name: "subject#{num}", pin: '1234')
+    subject = Subject.create!(name: "subject#{num + 1}", pin: '1234')
 
     SubjectDatum.create!(subject: subject, payload: example_json_1)
     SubjectDatum.create!(subject: subject, payload: example_json_2)
+    SubjectDatum.create!(subject: subject, payload: example_json_3)
   end
 end
 
@@ -13,4 +14,8 @@ end
 
 def example_json_2
   "{\"data\":{\"name\":\"subject\",\"message\":\"#{[1,2,3,4,5,6,7].sample}\"}}"
+end
+
+def example_json_3
+  "{\"data\":{\"message\":\"foo\",\"object\":{\"nested_message\":\"bar\",\"overly_nested_object\":{\"overly_nested_message\":\"you found me.\"}}}}"
 end
