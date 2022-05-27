af78545408771d54c5fc37a1a0f5786d8ad87ecc
Frechfuchs <dev.frechfuchs@gmail.com>
2022-05-27 16:58:51 +0200

    SubjectDataController show error wrong payload format

--- app/controllers/api/v1/subject_data_controller.rb
+++ app/controllers/api/v1/subject_data_controller.rb
@@ -2,17 +2,20 @@ class Api::V1::SubjectDataController < Api::V1::BaseController
   def create
     @subject = Subject.find_by(name: permitted_params[:name])
 
-    if is_authorized?
-      @subject_datum = SubjectDatum.new(payload: permitted_param_payload, subject: @subject)
+    if !is_authorized?
+      return render json: { status: "error", error: 'not_found' }
+    end
 
-      if @subject_datum.save
-        render json: { status: "ok", message: "Success" }
-      else
-        render json: { status: "error", errors: subject_datum.errors }
-      end
+    if !has_permitted_payload?
+      return render json: { status: "error", error: 'unpermitted_payload' }
+    end
 
+    @subject_datum = SubjectDatum.new(payload: permitted_param_payload, subject: @subject)
+
+    if @subject_datum.save
+      render json: { status: "ok", message: "Success" }
     else
-      render json: { status: "error", error: 'not_found' }
+      render json: { status: "error", errors: subject_datum.errors }
     end
   end
 
@@ -27,7 +30,15 @@ class Api::V1::SubjectDataController < Api::V1::BaseController
 
   def is_authorized?
     begin
-      @subject && @subject.pin == permitted_params[:pin] && permitted_param_payload.include?(:data)
+      @subject && @subject.pin == permitted_params[:pin]
+    rescue
+      false
+    end
+  end
+
+  def has_permitted_payload?
+    begin
+      permitted_param_payload.include?(:data)
     rescue
       false
     end
